4f0adf4456112aae609b515e32b3eb3a83eda1a0
AUnlocking <unlockaldaz@gmail.com>
2025-06-03 05:18:13 +0000

    commit

--- config/app.php
+++ config/app.php
@@ -7,9 +7,8 @@ return [
     | Application Name
     |--------------------------------------------------------------------------
     |
-    | This value is the name of your application, which will be used when the
-    | framework needs to place the application's name in a notification or
-    | other UI elements where an application name needs to be displayed.
+    | This value is el nombre de tu aplicación, que se usa en notificaciones
+    | u otros elementos de UI donde se muestra el nombre de la app.
     |
     */
 
@@ -20,9 +19,7 @@ return [
     | Application Environment
     |--------------------------------------------------------------------------
     |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services the application utilizes. Set this in your ".env" file.
+    | Define el entorno en el que tu aplicación se ejecuta (local, producción, etc.).
     |
     */
 
@@ -33,9 +30,8 @@ return [
     | Application Debug Mode
     |--------------------------------------------------------------------------
     |
-    | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
-    | application. If disabled, a simple generic error page is shown.
+    | En modo debug, los mensajes de error detallados con rastros se muestran.
+    | Si está deshabilitado, solo se verá una página de error genérica.
     |
     */
 
@@ -46,9 +42,7 @@ return [
     | Application URL
     |--------------------------------------------------------------------------
     |
-    | This URL is used by the console to properly generate URLs when using
-    | the Artisan command line tool. You should set this to the root of
-    | the application so that it's available within Artisan commands.
+    | La URL base de la aplicación utilizada por Artisan para generar enlaces.
     |
     */
 
@@ -59,9 +53,7 @@ return [
     | Application Timezone
     |--------------------------------------------------------------------------
     |
-    | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
+    | Define la zona horaria utilizada por la aplicación.
     |
     */
 
@@ -72,9 +64,7 @@ return [
     | Application Locale Configuration
     |--------------------------------------------------------------------------
     |
-    | The application locale determines the default locale that will be used
-    | by Laravel's translation / localization methods. This option can be
-    | set to any locale for which you plan to have translation strings.
+    | Define el idioma predeterminado de la aplicación.
     |
     */
 
@@ -89,9 +79,7 @@ return [
     | Encryption Key
     |--------------------------------------------------------------------------
     |
-    | This key is utilized by Laravel's encryption services and should be set
-    | to a random, 32 character string to ensure that all encrypted values
-    | are secure. You should do this prior to deploying the application.
+    | Esta clave es utilizada por los servicios de cifrado de Laravel.
     |
     */
 
@@ -107,14 +95,33 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Debug Hide
+    |--------------------------------------------------------------------------
+    |
+    | Oculta información sensible cuando APP_DEBUG está activado.
+    |
+    */
+
+    'debug_hide' => [
+        '_ENV' => [
+            'APP_KEY',
+            'DB_PASSWORD',
+        ],
+        '_SERVER' => [
+            'APP_KEY',
+            'DB_PASSWORD',
+        ],
+        '_POST' => [
+            'password',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | Maintenance Mode Driver
     |--------------------------------------------------------------------------
     |
-    | These configuration options determine the driver used to determine and
-    | manage Laravel's "maintenance mode" status. The "cache" driver will
-    | allow maintenance mode to be controlled across multiple machines.
-    |
-    | Supported drivers: "file", "cache"
+    | Controla cómo Laravel maneja el modo de mantenimiento.
     |
     */
 
@@ -122,5 +129,4 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
 ];
